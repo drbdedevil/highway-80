fa9f833e54caab16190b803ae6d842285d6b916c
ku4eriaviy <kozinegor10@gmail.com>
2026-05-16 16:55:16 +0500

    main menu buttons

--- Scenes/MainMenuScene.cs
+++ Scenes/MainMenuScene.cs
@@ -5,9 +5,31 @@ public partial class MainMenuScene : Node
 {
 	public override void _Ready()
 	{
+		Button exitButton = GetNode<Button>("MarginContainer/VBoxContainer/ExitButton");
+		exitButton.Pressed += OnExitButtonPressed;
+
+		Button startButton = GetNode<Button>("MarginContainer/VBoxContainer/StartButton");
+		startButton.Pressed += OnStartButtonPressed;
+		
+		Button settingsButton = GetNode<Button>("MarginContainer/VBoxContainer/SettinsButton");
+		settingsButton.Pressed += OnSettingsButtonPressed;
 	}
 
 	public override void _Process(double delta)
+	{
+	}
+
+	private void OnExitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+
+	private void OnStartButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/MainScene.tscn");
+	}
+
+	private void OnSettingsButtonPressed()
 	{
 	}
 }
