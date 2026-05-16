0c4e406cdabcbb8f9e5cfeae4a3b48bdc800b90c
ku4eriaviy <kozinegor10@gmail.com>
2026-05-16 23:19:15 +0500

    sounds main menu

--- Scenes/MainMenuScene.cs
+++ Scenes/MainMenuScene.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class MainMenuScene : Control
 {
+
+	private AudioStreamPlayer _clickSound;
 	public override void _Ready()
 	{
 		Button exitButton = GetNode<Button>("MarginContainer/VBoxContainer/ExitButton");
@@ -10,12 +12,27 @@ public partial class MainMenuScene : Control
 
 		Button startButton = GetNode<Button>("MarginContainer/VBoxContainer/StartButton");
 		startButton.Pressed += OnStartButtonPressed;
+
+		_clickSound = GetNode<AudioStreamPlayer>("ClickSound");
 		
 		SetupVolumeSliders();
 	}
 
 	public override void _Process(double delta)
 	{
+	}
+
+	private void PlayClickSound(Action action)
+	{
+		if (_clickSound != null)
+		{
+			_clickSound.Play();
+			var timer = GetTree().CreateTimer(0.3f);
+			timer.Timeout += () =>
+			{
+				action();
+			};
+		}
 	}
 
 	private void SetupVolumeSliders()
@@ -32,12 +49,12 @@ public partial class MainMenuScene : Control
 
 	private void OnExitButtonPressed()
 	{
-		GetTree().Quit();
+		PlayClickSound(() => GetTree().Quit());		
 	}
 
 	private void OnStartButtonPressed()
 	{
-		GetTree().ChangeSceneToFile("res://Scenes/TestScene.tscn");
+		PlayClickSound(() => GetTree().ChangeSceneToFile("res://Scenes/TestScene.tscn"));	
 	}
 
 }
